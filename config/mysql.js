29a8b4ec66b8470db29fceaecc13c2d2cbd75dbe
Higo Ribeiro <HORBJN@gmail.com>
2019-05-02 23:06:42 -0300

    Fixed mysql config

--- config/mysql.js
+++ config/mysql.js
@@ -2,6 +2,6 @@ module.exports = {
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASS,
   database: process.env.MYSQL_NAME,
-  server: process.env.MYSQL_SERVER || 'local',
+  server: process.env.MYSQL_SERVER || 'localhost',
   port: process.env.MYSQL_PORT || 3306
 }
