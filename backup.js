3121f9bcb928a25b1898d403b9fcc6182cf8a147
Higo Ribeiro <HORBJN@gmail.com>
2019-05-03 17:54:53 -0300

    Added cron to manage task automation

---
+++ backup.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const StorageManager = require('@slynova/flydrive')
+const Sentry = require('@sentry/node')
+
+const getMySQLData = require('./get-mysql-data')
+const now = require('./now')
+
+
+const configStorage = require('./config/storage')
+const configSentry = require('./config/sentry')
+const configMysql = require('./config/mysql')
+
+Sentry.init(configSentry);
+
+async function backup() {
+  try {
+    const storage = new StorageManager(configStorage)
+    await getMySQLData(configMysql, async (filename, stream) => {
+      await storage.put(filename, stream)
+    })
+  } catch (err) {
+    Sentry.captureException(err)
+  }
+}
+
+module.exports = backup
--- index.js
+++ index.js
@@ -1,29 +1,9 @@
+const CronJob = require('cron').CronJob
 const dotenv = require('dotenv')
-
-const fs = require('fs')
-const StorageManager = require('@slynova/flydrive')
-const Sentry = require('@sentry/node')
-
-const getMySQLData = require('./get-mysql-data')
-const now = require('./now')
-
 dotenv.config()
 
-const configStorage = require('./config/storage')
-const configSentry = require('./config/sentry')
-const configMysql = require('./config/mysql')
+const backup = require('./backup')
 
-Sentry.init(configSentry);
-
-async function main() {
-  try {
-    const storage = new StorageManager(configStorage)
-    await getMySQLData(configMysql, async (filename, stream) => {
-      await storage.put(filename, stream)
-    })
-  } catch (err) {
-    Sentry.captureException(err)
-  }
-}
-
-main()
+new CronJob('0 0 18 * * *', () => {
+  backup()
+}, null, true, 'America/Sao_Paulo')
